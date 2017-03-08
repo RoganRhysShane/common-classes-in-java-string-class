344ed0c24aca94de1fb8b594804b53a5aeef34b2
RoganRhysShane <chrisoliver08@live.com>
2017-03-08 11:57:49 -0500

    done

--- src/main/java/com/theironyard/StringKatas.java
+++ src/main/java/com/theironyard/StringKatas.java
@@ -15,8 +15,22 @@ package com.theironyard;
  * You can see all of CodingBat's exercises at http://codingbat.com/.
  *
  * Each of the katas below are focused on string manipulation.
+ *
+ *
+ * Variable = " jashagha"
+ * String name = "Bob"
+ * String name = "frank"
+ *
  */
+
+
 public class StringKatas {
+
+
+
+
+
+
 
     /**
      * Create a publicly accessible static method that does the following:
@@ -37,6 +51,16 @@ public class StringKatas {
      * @return A string greeting the name. EG: "Hello Bob!"
      */
     // todo: implement helloName() method
+
+
+      public static String helloName(String name){
+
+          return "Hello " + name + "!";
+
+        }
+
+
+
 
     /**
      * Create a publicly accessible static method that does the following:
@@ -62,48 +86,73 @@ public class StringKatas {
      */
     // todo: implement makeOutWord() method
 
-    /**
-     * Create a publicly accessible static method that does the following:
-     *
-     * Given a string of even length, return the first half. So the string
-     * "WooHoo" yields "Woo".
-     *
-     * firstHalf("WooHoo") → "Woo"
-     * firstHalf("HelloThere") → "Hello"
-     * firstHalf("abcdef") → "abc"
-     *
-     * This exercise is on CodingBat at http://codingbat.com/prob/p172267
-     *
-     * @param str The string to return the first half of.
-     * @return The first half of the string provided
-     */
-    // todo: implement firstHalf() method
+    public static String makeOutWord(String out, String word) {
+        return out.substring(0, 2) + word + out.substring(2, 4);
+    }
+
+        /**
+         * Create a publicly accessible static method that does the following:
+         *
+         * Given a string of even length, return the first half. So the string
+         * "WooHoo" yields "Woo".
+         *
+         * firstHalf("WooHoo") → "Woo"
+         * firstHalf("HelloThere") → "Hello"
+         * firstHalf("abcdef") → "abc"
+         *
+         * This exercise is on CodingBat at http://codingbat.com/prob/p172267
+         *
+         * @param str The string to return the first half of.
+         * @return The first half of the string provided
+         */
+        // todo: implement firstHalf() method
+   public static String firstHalf(String str){
+       return str.substring(0, str.length() /2);
+
+    }
+
 
     /**
-     * Create a publicly accessible static method that does the following:
-     *
-     * Given a string and an int n, return a string made of the first and last n
-     * chars from the string. The string length will be at least n.
-     *
-     * nTwice("Hello", 2) → "Helo"
-     * nTwice("Chocolate", 3) → "Choate"
-     * nTwice("Chocolate", 1) → "Ce"
-     *
-     * This exercise is on CodingBat at http://codingbat.com/prob/p174148
-     *
-     * Hint: With problems like this it is often useful to break it down into
-     * a process (also known as an algorithm). Maybe you could attack this
-     * problem by first getting the first n characters and making sure that
-     * works as expected. Then get the last n characters and make sure that
-     * works as expected. Finally, put them together for the correct result.
-     * Keep in mind that failing tests show you what was expected vs. what was
-     * actually received. You can use this to check incremental progress towards
-     * a solution.
-     *
-     * @param str The string to extract the first/last characters from
-     * @param n The number of characters to extract.
-     * @return A string made up of the first n and last n characters of `str` concatenated together
-     */
-    // todo: implement nTwice() method
+         * Create a publicly accessible static method that does the following:
+         *
+         * Given a string and an int n, return a string made of the first and last n
+         * chars from the string. The string length will be at least n.
+         *
+         * nTwice("Hello", 2) → "Helo"
+         * nTwice("Chocolate", 3) → "Choate"
+         * nTwice("Chocolate", 1) → "Ce"
+         *
+         * This exercise is on CodingBat at http://codingbat.com/prob/p174148
+         *
+         * Hint: With problems like this it is often useful to break it down into
+         * a process (also known as an algorithm). Maybe you could attack this
+         * problem by first getting the first n characters and making sure that
+         * works as expected. Then get the last n characters and make sure that
+         * works as expected. Finally, put them together for the correct result.
+         * Keep in mind that failing tests show you what was expected vs. what was
+         * actually received. You can use this to check incremental progress towards
+         * a solution.
+         *
+         * @param str The string to extract the first/last characters from
+         * @param n The number of characters to extract.
+         * @return A string made up of the first n and last n characters of `str` concatenated together
+         */
+
+    //Method always has this <access> <return type> <name>(){}
+    //public String sweetName() {}
+    //sweetName
+
+        // todo: implement nTwice() method
+    public static String nTwice(String str, int n){
+
+
+
+        return str.substring(0,n)  + str.substring(str.length()-n, str.length());
+
+       
+
+    }
+
 
 }
+
